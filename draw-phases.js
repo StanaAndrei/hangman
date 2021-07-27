ddc9ba453a7a4cc0a9542a949e94ec464263e889
StanaAndrei <stadey33@gmail.com>
2021-07-27 14:50:52 +0300

    game ready

---
+++ draw-phases.js
@@ -0,0 +1,65 @@
+const drawMainPilar = (context, w, h) => {
+    context.moveTo(w / 4, h);
+    context.lineTo(w / 4, h / 4);
+}
+
+const drawHorizPilar = (context, w, h) => {
+    context.moveTo(w / 4, h / 4);
+    context.lineTo(w - w / 4, h / 4);
+}
+
+let radius, headX, headY;
+const drawHead = (context, w, h) => {
+    radius = 35, headX = w - w / 4, headY = h / 2;
+    context.arc(headX, headY, radius, 0, 2 * Math.PI);
+    context.stroke();
+    context.beginPath();
+    context.arc(360, 236, 2, 0, 2 * Math.PI);
+    context.fill();
+    context.stroke();
+    context.beginPath();
+    context.arc(388, 236, 2, 0, 2 * Math.PI);
+    context.fill();
+    context.stroke();
+    context.moveTo(361, 265);
+    context.lineTo(390, 265);//*/
+    context.stroke();
+}
+
+const BODY_HEIGHT = 80, BODY_WIDTH = 13;
+const drawBody = (context, w, h) => {
+    context.fillRect(w - w / 4 - 5, h / 2 + radius, BODY_WIDTH, BODY_HEIGHT);
+}
+
+const drawLeftHand = (context, w, h) => {
+    context.moveTo(370, 300);
+    context.lineTo(335, 324);
+    context.lineWidth = 5;
+}
+
+const drawRightHand = (context, w, h) => {
+    context.moveTo(382, 300);
+    context.lineTo(420, 324);
+    context.lineWidth = 5;
+}
+
+const drawLeftLeg = (context, w, h) => {
+    context.moveTo(374, 366);
+    context.lineTo(362, 397);
+    context.lineWidth = 8;
+}
+
+const drawRightLeg = (context, w, h) => {
+    context.moveTo(380, 366);
+    context.lineTo(390, 397);
+    context.lineWidth = 8;
+}
+
+const drawRope = (context, w, h) => {
+    context.moveTo(w - w / 4, h / 4);
+    context.lineTo(headX, headY - radius);
+    context.lineWidth = 3;
+}
+
+const drawPhases = [drawMainPilar, drawHorizPilar, drawHead, drawBody, drawLeftHand, drawRightHand, drawLeftLeg, drawRightLeg, drawRope];
+export default drawPhases;
---
+++ script.js
@@ -0,0 +1,119 @@
+import drawPhases from "./draw-phases.js";
+const DEBUG = false;
+const pre = document.querySelector('#setup');
+const input = document.querySelector('input');
+const submitBtn = document.querySelector('#submit');
+const main = document.querySelector('#main');
+const lettersDiv = document.querySelector('#letters');
+const fieldsDiv = document.querySelector('#fields');
+let word = new String();
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+context.fillStyle = 'black';
+context.strokeStyle = 'black';
+
+const updateFields = letter => {
+    if (letter.length != 1) {
+        throw new Error('the param is a string!!!');
+        //in case I send a string as param instead of a letter
+    }
+    let fieldsArr = document.querySelectorAll('#field');
+    let found = false;
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            fieldsArr[i].value = letter;
+            found = true;
+        }
+    }
+    return found;
+};
+
+const checkWin = () => {
+    let fieldsArr = document.querySelectorAll('#field');
+    for (let i = 0; i < fieldsArr.length; i++) {
+        if (!fieldsArr[i].value.length) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const finishGame = win => {
+    let italic = document.querySelector('i');
+    if (win) {
+        italic.style.color = 'rgb(0, 252, 67)';
+        italic.innerText = 'YOU WON!';
+    } else {
+        italic.style.color = 'red';
+        italic.innerText = 'YOU LOST!';
+    }
+    lettersDiv.innerHTML = '';
+};
+
+let phase = 0;
+const handleLetterBtnClick = (e) => {
+    e.preventDefault();
+    let btn = e.target;
+    btn.disabled = true;
+    let letter = btn.innerText;
+    let ok = updateFields(letter);
+    if (checkWin()) {
+        finishGame(true);
+        return;
+    }
+    if (!ok) {
+        context.beginPath();
+        drawPhases[phase++](context, canvas.width, canvas.height);
+        context.stroke();
+        if (phase === drawPhases.length) {
+            //lost
+            finishGame(false);
+        }
+    }
+};
+
+const load = () => {
+    //load letter-buttons
+    const FIRST = 'a'.charCodeAt(0);
+    const LAST = 'z'.charCodeAt(0);
+    for (let chCode = FIRST; chCode <= LAST; chCode++) {
+        let btn = document.createElement('button');
+        const ch = String.fromCharCode(chCode);
+        let text = document.createTextNode(`${ch}`);
+        btn.appendChild(text);
+        btn.addEventListener('click', handleLetterBtnClick);
+        if (word[0] === String(ch) || word.slice(-1) === String(ch)) {
+            btn.disabled = true;
+        }
+        lettersDiv.appendChild(btn);
+    }
+    //load letter-fields
+    for (let i = 0; i < word.length; i++) {
+        let letterInp = document.createElement('input');
+        letterInp.disabled = true;
+        letterInp.maxLength = 1;
+        letterInp.id = 'field';
+        fieldsDiv.appendChild(letterInp);
+    }
+    //console.clear();
+};
+
+submitBtn.addEventListener('click', e => {
+    e.preventDefault();
+    word = input.value;
+    pre.style.display = 'none';
+    load();
+    main.style.display = 'block';
+    updateFields(word[0]);
+    updateFields(word.slice(-1));
+});
+
+
+if (DEBUG) {
+    canvas.addEventListener('click', e => {
+        let rect = canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left, y = e.clientY - rect.top;
+        console.log(x, y);
+        /*use this to get coordinates of hands/legs to know where to draw them*/
+    });
+}
